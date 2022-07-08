b8a98deb51858669fe03779dcd33846a3fc69a26
stavbers <reddevweb@gmail.com>
2022-07-08 04:41:14 +0300

    add basik styles and html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,3 +7,24 @@ document.addEventListener('DOMContentLoaded', function(){
   
 })
 
+let date = new Date("1,1,2023")
+
+// let CountDay = document.getElementById("Days")
+// let CountHour = document.getElementById("Hours")
+// let CountMinutes = document.getElementById("Minutes")
+// let CountSeconds = document.getElementById("Seconds")
+let Int = setInterval(updateTime, 1000)
+
+function updateTime() {
+  let Now = new Date().getTime()
+  let distance = date - Now
+  let day, hour, min, sec
+  day = Math.floor(distance / (1000 * 60 * 60 * 24));
+  hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  sec = Math.floor((distance % (1000 * 60)) / 1000);
+
+  console.log(day, hour, min, sec);
+
+
+  }
