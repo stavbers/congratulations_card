384fe85e8fc125f6a5c22d533766ef90cc8b5484
stavbers <reddevweb@gmail.com>
2022-07-10 08:13:42 +0300

    refact new html and add new css, refactor js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const checkInpYear = value => value.trim() > 1906 && value.trim() < new Date().getFullYear() ? true : false
   const removeElem = (el) => el.forEach(el => el.remove())
   const clearInp = (day, month, year) => {
-    day.value = '';
-    month.value = '';
+    day.value = ''
+    month.value = ''
     year.value = ''
   }
 
@@ -27,22 +27,25 @@ document.addEventListener('DOMContentLoaded', () => {
           </div>
         </div>`)
   const app = createElem(`
-  <div class="app-wrp">
+  <div class="app-wrp"> 
   <h2 class="app-wrp__title">Счетчик до даты дня рождения</h2>
   <div class="app-wrp__countdown countdown-wrp">
-    <p class="countdown-wrp__age">Вам исполнится 31 год</p>
+    <p class="countdown-wrp__age">Вам исполнится <span>31</span> год</p>
     <h2 class="countdown-wrp__title">До дня рождения осталось:</h2>
-    <div class="countdown-wrp__inner">
-      <p class="countdown-wrp__month">22</p>
-      <p class="countdown-wrp__week">22</p>
-      <p class="countdown-wrp__weekend">22</p>
-      <p class="countdown-wrp__workDay">22</p>
-      <p class="countdown-wrp__days">22</p>
-      <p class="countdown-wrp__hours">22</p>
-      <p class="countdown-wrp__minutes">22</p>
-      <p class="countdown-wrp__seconds">22</p>
+    <div class="countdown-wrp__inner wrp-inner">
+      <div class="wrp-inner__left">
+        <p class="countdown-wrp__month">5 месяцев</p>
+        <p class="countdown-wrp__workDay">98 рабочих дней</p>
+        <p class="countdown-wrp__days">137 дней</p>
+        <p class="countdown-wrp__minutes">51 минута</p>
+      </div>
+      <div class="wrp-inner__right">
+        <p class="countdown-wrp__week">19.5 недель</p>
+        <p class="countdown-wrp__weekend">39 выходных дней</p>
+        <p class="countdown-wrp__hours">17 часов</p>
+        <p class="countdown-wrp__seconds">52 секунды</p>
+      </div>
     </div>
-
   </div>
   <h4 class="app-wrp__subtitle">Текст поздравления</h4>
   <div class="app-wrp__note">
@@ -77,47 +80,43 @@ document.addEventListener('DOMContentLoaded', () => {
     const dob = new Date(year, month - 1, day)
     let dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate())
     const age = today.getFullYear() - dob.getFullYear()
-    const html = document.querySelector('.app-wrp__countdown span')
-    html.textContent = `Вам исполнится ${age} лет 
-    до дня рождения осталось `
+    const html = document.querySelector('.countdown-wrp__age')
+    html.textContent = `Вам исполнится ${age} ${num_word(age, ['год', 'года', 'лет'])}`
     if (month < new Date().getMonth()) dobnow = new Date(today.getFullYear() + 1, dob.getMonth(), dob.getDate())
-    // console.log(new Date().getMonth() < )
     setInterval(countTimer.bind(dobnow), 1000);
   }
 
 
 
   function countTimer() {
-    const timerOut = document.querySelector('.app-wrp__countdown')
+    const monthOut = document.querySelector('.countdown-wrp__month')
+    const weekOut = document.querySelector('.countdown-wrp__week')
+    const weekendOut = document.querySelector('.countdown-wrp__weekend')
+    const workDayOut = document.querySelector('.countdown-wrp__workDay')
+    const daysOut = document.querySelector('.countdown-wrp__days')
+    const hoursOut = document.querySelector('.countdown-wrp__hours')
+    const minutesOut = document.querySelector('.countdown-wrp__minutes')
+    const secondsOut = document.querySelector('.countdown-wrp__seconds')
     let milsTime = Math.abs(new Date() - new Date(this))
-    let month = milsTime > 0 ? Math.round(milsTime / 1000 / 60 / 60 / 24 / 30) : 0;
-    let week = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7) : 0;
-    let weekend = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7 * 2) : 0;
-    let workDay = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7 * 5) : 0;
-    let days = milsTime > 0 ? Math.round(milsTime / 1000 / 60 / 60 / 24) : 0;
-    let hours = milsTime > 0 ? Math.floor(milsTime / 1000 / 60 / 60) % 24 : 0;
-    let minutes = milsTime > 0 ? Math.floor(milsTime / 1000 / 60) % 60 : 0;
-    let seconds = milsTime > 0 ? Math.floor(milsTime / 1000) % 60 : 0;
-
-
-
-    timerOut.textContent =
-      `
-   ${month} ${num_word(month, ['месяц', 'месяца', 'месяцев'])}
-   ${week.toFixed(1)} ${num_word(week.toFixed(1), ['недели', 'неделя', 'недель'])}
-   ${workDay.toFixed()} ${num_word(workDay.toFixed(), ['рабочих дней', 'рабочих дней', 'рабочих дней'])}
-   ${weekend.toFixed()} ${num_word(weekend.toFixed(), ['выходных дней', 'выходных дней', 'выходных дней'])}
-   ${days} ${num_word(days, ['день', 'дня', 'дней'])}
-   ${hours} ${num_word(hours, ['час', 'часа', 'часов'])}
-   ${minutes} ${num_word(minutes, ['минута', 'минуты', 'минут'])}
-   ${seconds} ${num_word(seconds, ['секунда', 'секунды', 'секунд'])}
-  `
-
+    let month = milsTime > 0 ? Math.round(milsTime / 1000 / 60 / 60 / 24 / 30) : 0
+    let week = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7) : 0
+    let weekend = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7 * 2) : 0
+    let workDay = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7 * 5) : 0
+    let days = milsTime > 0 ? Math.round(milsTime / 1000 / 60 / 60 / 24) : 0
+    let hours = milsTime > 0 ? Math.floor(milsTime / 1000 / 60 / 60) % 24 : 0
+    let minutes = milsTime > 0 ? Math.floor(milsTime / 1000 / 60) % 60 : 0
+    let seconds = milsTime > 0 ? Math.floor(milsTime / 1000) % 60 : 0
+    monthOut.textContent = `${month} ${num_word(month, ['месяц', 'месяца', 'месяцев'])}`
+    weekOut.textContent = `${num_word(week.toFixed(), ['недели', 'неделя', 'недель'])} ${week.toFixed(1)}`
+    weekendOut.textContent = `${num_word(weekend.toFixed(), ['выходных дней', 'выходных дней', 'выходных дней'])} ${weekend.toFixed()}`
+    workDayOut.textContent = `${workDay.toFixed()} ${num_word(workDay.toFixed(), ['рабочих дней', 'рабочих дней', 'рабочих дней'])}`
+    daysOut.textContent = `${days} ${num_word(days, ['день', 'дня', 'дней'])}`
+    hoursOut.textContent = `${num_word(hours, ['час', 'часа', 'часов'])} ${hours}`
+    minutesOut.textContent = `${minutes} ${num_word(minutes, ['минута', 'минуты', 'минут'])}`
+    secondsOut.textContent = `${num_word(seconds, ['секунда', 'секунды', 'секунд'])} ${seconds}`
   }
 
-
-
-  function num_word(number, titles) {
+  const num_word = (number, titles) => {
     cases = [2, 0, 1, 1, 1, 2];
     return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
   }
