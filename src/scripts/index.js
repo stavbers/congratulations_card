e81d61662ff74d15ab233305b798e45a819094b9
stavbers <reddevweb@gmail.com>
2022-07-10 06:42:52 +0300

    add new html(countdown-wrp)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,9 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const checkInputValue = (day, month, year) => {
-    (checkInpDay(day.value) && checkInpMonth(month.value) && checkInpYear(year.value)) ?
-    [removeElem([document.querySelector('#overlay'), document.querySelector('.popup')]), addElem(app, body.querySelector('#App')), getAge(day.value, month.value, year.value)] :
-    [clearInp(day, month, year), false]
+    (checkInpDay(day.value) && checkInpMonth(month.value) && checkInpYear(year.value)) ? [removeElem([document.querySelector('#overlay'), document.querySelector('.popup')]), addElem(app, body.querySelector('#App')), getAge(day.value, month.value, year.value)] : [clearInp(day, month, year), false]
   }
   const createElem = elem => `${elem}`
   const addElem = (elem, out = body) => out.insertAdjacentHTML('beforeend', elem)
@@ -29,15 +27,29 @@ document.addEventListener('DOMContentLoaded', () => {
           </div>
         </div>`)
   const app = createElem(`
-        <div class="app-wrp">
-          <h2 class="app-wrp__title">Birthday Countdown</h2>
-          <p class="app-wrp__countdown">Осталось 24 дня 40 часов 59 минут и 6 сек</p>
-          <h4 class="app-wrp__subtitle">text felicitation</h4>
-          <div class="app-wrp__note">
-            <div class="saved">Saved...</div>
-            <textarea name="text" id="felic-text" cols="30" rows="10"></textarea>
-            </div>
-          </div>`)
+  <div class="app-wrp">
+  <h2 class="app-wrp__title">Счетчик до даты дня рождения</h2>
+  <div class="app-wrp__countdown countdown-wrp">
+    <p class="countdown-wrp__age">Вам исполнится 31 год</p>
+    <h2 class="countdown-wrp__title">До дня рождения осталось:</h2>
+    <div class="countdown-wrp__inner">
+      <p class="countdown-wrp__month">22</p>
+      <p class="countdown-wrp__week">22</p>
+      <p class="countdown-wrp__weekend">22</p>
+      <p class="countdown-wrp__workDay">22</p>
+      <p class="countdown-wrp__days">22</p>
+      <p class="countdown-wrp__hours">22</p>
+      <p class="countdown-wrp__minutes">22</p>
+      <p class="countdown-wrp__seconds">22</p>
+    </div>
+
+  </div>
+  <h4 class="app-wrp__subtitle">Текст поздравления</h4>
+  <div class="app-wrp__note">
+    <div class="saved">Сохранено...</div>
+    <textarea name="text" id="felic-text" cols="30" rows="10"></textarea>
+  </div>
+</div>`)
   addElem(overlay)
   addElem(popup)
   const overlayEl = document.querySelector('#overlay')
@@ -47,14 +59,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const inpDay = document.querySelector('#p-inpDay')
     const inpMon = document.querySelector('#p-inpMon')
     const inpYear = document.querySelector('#p-inpY')
-    // const cd = document.querySelector('.app-wrp__countdown')
-    // const dateArr = []
     checkInputValue(inpDay, inpMon, inpYear)
   })
 
   document.addEventListener('keydown', (e) => {
-    e.code == 'Escape' && document.querySelector('#p-inpY') ?
-      [removeElem([overlayEl, popupEl]), addElem(app, body.querySelector('#App'))] :
+    e.code == 'Escape' && document.querySelector('#p-inpY') ? [removeElem([overlayEl, popupEl]), addElem(app, body.querySelector('#App'))] :
       false
   })
   overlayEl.addEventListener('click', (e) => {
@@ -63,16 +72,15 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   const getAge = (day, month, year) => {
-   
-    const now = new Date() 
-    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate()) 
-    const dob = new Date(year, month - 1, day) 
-    let dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate()) 
+    const now = new Date()
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+    const dob = new Date(year, month - 1, day)
+    let dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate())
     const age = today.getFullYear() - dob.getFullYear()
-    let html = document.querySelector('.app-wrp__countdown')
+    const html = document.querySelector('.app-wrp__countdown span')
     html.textContent = `Вам исполнится ${age} лет 
     до дня рождения осталось `
-    if(month < new Date().getMonth()) dobnow = new Date(today.getFullYear() + 1, dob.getMonth(), dob.getDate()) 
+    if (month < new Date().getMonth()) dobnow = new Date(today.getFullYear() + 1, dob.getMonth(), dob.getDate())
     // console.log(new Date().getMonth() < )
     setInterval(countTimer.bind(dobnow), 1000);
   }
@@ -80,23 +88,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   function countTimer() {
-    let milsTime = Math.abs(new Date() - new Date(this));
-    if (milsTime <= 0) clearInterval(timerId1);
-    let year = milsTime > 0 ? Math.round(milsTime / 1000 / 60 / 60 / 24 / 365) : 0;
+    const timerOut = document.querySelector('.app-wrp__countdown')
+    let milsTime = Math.abs(new Date() - new Date(this))
     let month = milsTime > 0 ? Math.round(milsTime / 1000 / 60 / 60 / 24 / 30) : 0;
-  
     let week = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7) : 0;
     let weekend = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7 * 2) : 0;
     let workDay = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7 * 5) : 0;
-  
     let days = milsTime > 0 ? Math.round(milsTime / 1000 / 60 / 60 / 24) : 0;
     let hours = milsTime > 0 ? Math.floor(milsTime / 1000 / 60 / 60) % 24 : 0;
     let minutes = milsTime > 0 ? Math.floor(milsTime / 1000 / 60) % 60 : 0;
     let seconds = milsTime > 0 ? Math.floor(milsTime / 1000) % 60 : 0;
-  
 
-  let html = document.querySelector('.app-wrp__countdown')
-    html.textContent =
+
+
+    timerOut.textContent =
       `
    ${month} ${num_word(month, ['месяц', 'месяца', 'месяцев'])}
    ${week.toFixed(1)} ${num_word(week.toFixed(1), ['недели', 'неделя', 'недель'])}
@@ -107,22 +112,13 @@ document.addEventListener('DOMContentLoaded', () => {
    ${minutes} ${num_word(minutes, ['минута', 'минуты', 'минут'])}
    ${seconds} ${num_word(seconds, ['секунда', 'секунды', 'секунд'])}
   `
-  
-    // console.log(month)
-    // console.log(days)
-    // console.log(week.toFixed(1))
-    // console.log(workDay.toFixed())
-    // console.log(weekend.toFixed())
-    // console.log(hours.toFixed())
-    // console.log(minutes.toFixed())
-    // console.log(seconds.toFixed())
+
   }
 
 
 
-  function num_word(number, titles) {  
-    cases = [2, 0, 1, 1, 1, 2];  
-    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
-}
+  function num_word(number, titles) {
+    cases = [2, 0, 1, 1, 1, 2];
+    return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+  }
 })
-
