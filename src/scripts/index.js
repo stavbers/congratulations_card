fc9f3b7cf7c2e30034a828b1be6a1c725d8771ec
stavbers <reddevweb@gmail.com>
2022-07-10 06:15:10 +0300

    add worked fn countTimer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   const body = document.querySelector('body')
-  const checkInpDay = value => value.trim() > 0 && value.trim() < 32  ? true : false
-  const checkInpMonth = value => value.trim() > 0 && value.trim() < 13  ? true : false
+  const checkInpDay = value => value.trim() > 0 && value.trim() < 32 ? true : false
+  const checkInpMonth = value => value.trim() > 0 && value.trim() < 13 ? true : false
   const checkInpYear = value => value.trim() > 1906 && value.trim() < new Date().getFullYear() ? true : false
   const removeElem = (el) => el.forEach(el => el.remove())
   const clearInp = (day, month, year) => {
     day.value = '';
     month.value = '';
-    year.value = '' }
+    year.value = ''
+  }
 
-  const checkInputValue = (day, month, year) => { 
-    (checkInpDay(day.value) && checkInpMonth(month.value) && checkInpYear(year.value))
-     ? [removeElem([document.querySelector('#overlay'), document.querySelector('.popup')]), addElem(app, body.querySelector('#App'))] 
-     : [clearInp(day, month, year), false]
+  const checkInputValue = (day, month, year) => {
+    (checkInpDay(day.value) && checkInpMonth(month.value) && checkInpYear(year.value)) ?
+    [removeElem([document.querySelector('#overlay'), document.querySelector('.popup')]), addElem(app, body.querySelector('#App')), getAge(day.value, month.value, year.value)] :
+    [clearInp(day, month, year), false]
   }
   const createElem = elem => `${elem}`
   const addElem = (elem, out = body) => out.insertAdjacentHTML('beforeend', elem)
@@ -49,45 +50,79 @@ document.addEventListener('DOMContentLoaded', () => {
     // const cd = document.querySelector('.app-wrp__countdown')
     // const dateArr = []
     checkInputValue(inpDay, inpMon, inpYear)
-    countDown
   })
 
-  document.addEventListener('keydown', (e)=> {
-  e.code == 'Escape' && document.querySelector('#p-inpY')
-  ? [removeElem([overlayEl, popupEl]), addElem(app, body.querySelector('#App'))]
-  : false
-})
-  overlayEl.addEventListener('click', (e)=> {
-  removeElem([overlayEl, popupEl])
-  addElem(app, body.querySelector('#App'))
-})
+  document.addEventListener('keydown', (e) => {
+    e.code == 'Escape' && document.querySelector('#p-inpY') ?
+      [removeElem([overlayEl, popupEl]), addElem(app, body.querySelector('#App'))] :
+      false
+  })
+  overlayEl.addEventListener('click', (e) => {
+    removeElem([overlayEl, popupEl])
+    addElem(app, body.querySelector('#App'))
+  })
 
-const countDown = (arr)=> {
-  const date = new Date('12,24,1991')
+  const getAge = (day, month, year) => {
+   
+    const now = new Date() 
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate()) 
+    const dob = new Date(year, month - 1, day) 
+    let dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate()) 
+    const age = today.getFullYear() - dob.getFullYear()
+    let html = document.querySelector('.app-wrp__countdown')
+    html.textContent = `Вам исполнится ${age} лет 
+    до дня рождения осталось `
+    if(month < new Date().getMonth()) dobnow = new Date(today.getFullYear() + 1, dob.getMonth(), dob.getDate()) 
+    // console.log(new Date().getMonth() < )
+    setInterval(countTimer.bind(dobnow), 1000);
+  }
+
+
+
+  function countTimer() {
+    let milsTime = Math.abs(new Date() - new Date(this));
+    if (milsTime <= 0) clearInterval(timerId1);
+    let year = milsTime > 0 ? Math.round(milsTime / 1000 / 60 / 60 / 24 / 365) : 0;
+    let month = milsTime > 0 ? Math.round(milsTime / 1000 / 60 / 60 / 24 / 30) : 0;
+  
+    let week = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7) : 0;
+    let weekend = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7 * 2) : 0;
+    let workDay = milsTime > 0 ? (milsTime / 1000 / 60 / 60 / 24 / 7 * 5) : 0;
+  
+    let days = milsTime > 0 ? Math.round(milsTime / 1000 / 60 / 60 / 24) : 0;
+    let hours = milsTime > 0 ? Math.floor(milsTime / 1000 / 60 / 60) % 24 : 0;
+    let minutes = milsTime > 0 ? Math.floor(milsTime / 1000 / 60) % 60 : 0;
+    let seconds = milsTime > 0 ? Math.floor(milsTime / 1000) % 60 : 0;
+  
+
+  let html = document.querySelector('.app-wrp__countdown')
+    html.textContent =
+      `
+   ${month} ${num_word(month, ['месяц', 'месяца', 'месяцев'])}
+   ${week.toFixed(1)} ${num_word(week.toFixed(1), ['недели', 'неделя', 'недель'])}
+   ${workDay.toFixed()} ${num_word(workDay.toFixed(), ['рабочих дней', 'рабочих дней', 'рабочих дней'])}
+   ${weekend.toFixed()} ${num_word(weekend.toFixed(), ['выходных дней', 'выходных дней', 'выходных дней'])}
+   ${days} ${num_word(days, ['день', 'дня', 'дней'])}
+   ${hours} ${num_word(hours, ['час', 'часа', 'часов'])}
+   ${minutes} ${num_word(minutes, ['минута', 'минуты', 'минут'])}
+   ${seconds} ${num_word(seconds, ['секунда', 'секунды', 'секунд'])}
+  `
+  
+    // console.log(month)
+    // console.log(days)
+    // console.log(week.toFixed(1))
+    // console.log(workDay.toFixed())
+    // console.log(weekend.toFixed())
+    // console.log(hours.toFixed())
+    // console.log(minutes.toFixed())
+    // console.log(seconds.toFixed())
+  }
+
+
+
+  function num_word(number, titles) {  
+    cases = [2, 0, 1, 1, 1, 2];  
+    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
 }
-
-
 })
 
-// если день и месяц больше текущего то что-то делать
-
-
-const nowDate = new Date()
-console.log(date.getTime())
-console.log(nowDate.getTime())
-// console.log(date.getMonth())
-// setInterval(countDown, 1000)
-
-function countDown() {
-  let Now = new Date().getTime()
-  let distance = date - Now
-  let month, day, hour, min, sec
-  month = Math.floor(diff / 1000 / 60 / 60 / 31) % 12
-  day = Math.floor(distance / (1000 * 60 * 60 * 24));
-  hour = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  sec = Math.floor((distance % (1000 * 60)) / 1000);
-
-console.log(day,hour, min, sec)
-
-}
